8d7f746a4c1d419c04aa853396dd5b260e8ffce4
suryapradipta.github.io <63577853+suryapradipta@users.noreply.github.com>
2023-11-02 14:02:59 +0800

    ui progress: customer dashbaord

---
+++ src/app/pages/customer/dashboard/package/package.component.ts
@@ -0,0 +1,37 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-package',
+  templateUrl: './package.component.html',
+  styleUrls: ['./package.component.css']
+})
+export class PackageComponent {
+  packages = [
+    {
+      name: 'Diving',
+      href: '#',
+      imageSrc: 'assets/images/diving.jpg',
+    },
+    {
+      name: 'Cruise',
+      href: '#',
+      imageSrc: 'assets/images/cruise.jpg',
+    },
+    {
+      name: 'Honeymoon',
+      href: '#',
+      imageSrc: 'assets/images/honeymoon.jpg',
+
+    },
+    {
+      name: 'Homestay',
+      href: '#',
+      imageSrc: 'assets/images/homestay.jpg',
+    },
+    {
+      name: 'Shopping',
+      href: '#',
+      imageSrc: 'assets/images/shopping.jpg',
+    },
+  ]
+}
