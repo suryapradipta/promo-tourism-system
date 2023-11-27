b96f0ec1608e30804aa781753bf587953e589973
suryapradipta.github.io <63577853+suryapradipta@users.noreply.github.com>
2023-11-27 23:31:53 +0800

    config: server side, dependencies

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
+
+module.exports = app;
